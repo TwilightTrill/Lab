87341aa7e6dfb9e4aa146f1fd7856f9a024e5cfe
TwilightTrill <best.52300@gmail.com>
2026-02-19 22:52:50 +0700

    Edit2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'เย้ๆๆๆๆๆๆๆๆๆๆ ทำได้แล้วมั้งนะ';
+    return `
+    <div style="text-align:center; margin-top: 50px; font-family: Arial;">
+      <h1 style="color: #e0234e;">ยินดีด้วย! ระบบ CI/CD ทำงานแล้ว</h1>
+      <p style="font-size: 20px;">ข้อความจากโค้ด: <b>เย้ๆๆๆๆๆๆ ทำได้แล้ว!</b></p>
+      <img src="https://nestjs.com/img/logo-small.svg" width="100">
+    </div>
+  `;
   }
 }
